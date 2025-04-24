19c48bf48fbead893a4515df753fe43c6af54100
carlos_wendel <wendeluzumak824@gmail.com>
2025-04-23 23:28:04 -0300

    complementações css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton, Titulo } from "./styles";
 import logoImg from'../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionMoadal } from "../NewTransactionModal";
 
 export function Header(){
     return (
@@ -8,8 +10,12 @@ export function Header(){
 
                 <img src={logoImg} alt="" />
                <Titulo> DT Money</Titulo>
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
-
+              <Dialog.Root>
+                <Dialog.Trigger  asChild>
+                    <NewTransactionButton>Nova Transação</NewTransactionButton>
+                </Dialog.Trigger> 
+                    <NewTransactionMoadal/>
+              </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
